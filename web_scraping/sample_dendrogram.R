32103fa7b53bc9293678d695bf420382719a8ba7
paulapivat <paul.apivat@gmail.com>
2021-03-02 09:58:39 +0700

    edit sample_dendrogram

--- web_scraping/sample_dendrogram.R
+++ web_scraping/sample_dendrogram.R
@@ -1,6 +1,6 @@
 # Libraries
 library(ggraph)
-library(igraph)=
+library(igraph)
 library(tidyverse)
 theme_set(theme_void())
 library(RColorBrewer) 
@@ -29,6 +29,8 @@ ggraph(mygraph, layout = 'dendrogram', circular = FALSE) +
     ylim(-.4, NA) +
     coord_flip()
 
+# circular plot
+# geom_node_text NEED REFORMATTING
 ggraph(mygraph, layout = 'dendrogram', circular = TRUE) + 
     geom_edge_diagonal() +
     geom_node_text(aes( label=name, filter=leaf) , angle=90 , hjust=1, nudge_y = -0.04) +
@@ -36,7 +38,7 @@ ggraph(mygraph, layout = 'dendrogram', circular = TRUE) +
 
 
 # CIRCULAR PLOT ----
-set.seed(1)
+#set.seed(1)
 
 # create a data frame giving the hierarchical structure of your individuals
 d1=data.frame(from="origin", to=paste("group", seq(1,10), sep=""))
@@ -46,7 +48,7 @@ edges=rbind(d1, d2)
 # create a vertices data.frame. One line per object of our hierarchy
 vertices = data.frame(
     name = unique(c(as.character(edges$from), as.character(edges$to))) , 
-    value = runif(111)
+    #value = runif(111)
 ) 
 # Let's add a column with the group of each name. It will be useful later to color points
 vertices$group = edges$from[ match( vertices$name, edges$to ) ]
@@ -58,25 +60,30 @@ vertices$id=NA
 myleaves=which(is.na( match(vertices$name, edges$from) ))
 nleaves=length(myleaves)
 vertices$id[ myleaves ] = seq(1:nleaves)
-vertices$angle= 90 - 360 * vertices$id / nleaves
+#vertices$angle= 90 - 360 * vertices$id / nleaves
+vertices$angle = 360 / nleaves * vertices$id + 90
 
 # calculate the alignment of labels: right or left
 # If I am on the left part of the plot, my labels have currently an angle < -90
-vertices$hjust<-ifelse( vertices$angle < -90, 1, 0)
+#vertices$hjust<-ifelse( vertices$angle < -90, 1, 0)
+
+vertices$hjust<-ifelse( vertices$angle < 271, 1, 0)
 
 # flip angle BY to make them readable
-vertices$angle<-ifelse(vertices$angle < -90, vertices$angle+180, vertices$angle)
+#vertices$angle<-ifelse(vertices$angle < -90, vertices$angle+180, vertices$angle)
+
+vertices$angle<-ifelse(vertices$angle < 271, vertices$angle+180, vertices$angle)
 
 # Create a graph object
 mygraph <- graph_from_data_frame( edges, vertices=vertices )
 
 
 # Make the plot
-set.seed(1)
+#set.seed(1)
 ggraph(mygraph, layout = 'dendrogram', circular = TRUE) + 
     geom_edge_diagonal(colour="grey") +
     scale_edge_colour_distiller(palette = "RdPu") +
-    geom_node_text(aes(x = x*1.15, y=y*1.15, filter = leaf, label=name, angle = angle, hjust= hjust, colour=group), size=2.7, alpha=1) +
+    geom_node_text(aes(x = x*1.15, y=y*1.15, filter = leaf, label=name, angle = vertices$angle, hjust= vertices$hjust, colour=vertices$group), size=2.7, alpha=1) +
     geom_node_point(aes(filter = leaf, x = x*1.07, y=y*1.07, colour=group, size=value, alpha=0.2)) +
     scale_colour_manual(values= rep( brewer.pal(9,"Paired") , 30)) +
     scale_size_continuous( range = c(0.1,7) ) +
