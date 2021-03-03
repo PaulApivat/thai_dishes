b77fda7e7d3253e74475fdf31eca1ee7678f9689
paulapivat <paul.apivat@gmail.com>
2021-03-03 22:14:25 +0700

    add text mining code for exploreing word frequencies

--- text_mining/exploratory.R
+++ text_mining/exploratory.R
@@ -35,6 +35,78 @@ df %>%
 
 
 
+# Word Frequencies ----
+
+# visualize freq via scatterplot
+df %>%
+    select(Thai_name, Thai_script) %>%
+    unnest_tokens(ngrams, Thai_name) %>%  
+    group_by(ngrams) %>%  
+    tally(sort = TRUE) %>%  # alt: count(sort = TRUE)
+    ggplot(aes(n, ngrams)) + 
+    geom_point(aes(fill = ngrams)) +
+    theme(legend.position = "none")
+
+# Word Frequencies (Book Example)----
+install.packages('gutenbergr')
+library(gutenbergr)
+
+# HG Wells ----
+hgwells <- gutenberg_download(c(35, 36, 5230, 159))
+
+tidy_hgwells <- hgwells %>%
+    unnest_tokens(word, text) %>%
+    anti_join(stop_words)
+
+# most common words in H.G. Wells novel
+tidy_hgwells %>%
+    group_by(word) %>%
+    tally(sort = TRUE)
+
+tidy_hgwells %>%
+    count(word, sort = TRUE)
+
+# Bronte ----
+
+bronte <- gutenberg_download(c(1260, 768, 969, 9182, 767))
+
+tidy_bronte <- bronte %>%
+    unnest_tokens(word, text) %>%
+    anti_join(stop_words)
+
+tidy_bronte %>%
+    count(word, sort = TRUE)
+
+# Jane Austen ----
+library(janeaustenr)
+
+original_books <- austen_books() %>%
+    group_by(book) %>%
+    mutate(linenumber = row_number(),
+           chapter = cumsum(str_detect(text, 
+                                       regex("^chapter [\\divxlc]",
+                                             ignore_case = TRUE)))) %>%
+    ungroup()
+
+original_books
 
 
-?unnest_tokens
+tidy_books <- original_books %>%
+    unnest_tokens(word, text)
+
+# Frequency ----
+library(tidyr)
+
+frequency <- bind_rows(mutate(tidy_bronte, author = "Brontë Sisters"),
+                       mutate(tidy_hgwells, author = "H.G. Wells"), 
+                       mutate(tidy_books, author = "Jane Austen")) %>% 
+    mutate(word = str_extract(word, "[a-z']+")) %>%
+    count(author, word) %>%
+    group_by(author) %>%
+    mutate(proportion = n / sum(n)) %>% 
+    select(-n) %>% 
+    spread(author, proportion) %>% 
+    gather(author, proportion, `Brontë Sisters`:`H.G. Wells`)
+
+
+
