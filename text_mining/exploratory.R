59b86eb1bfea3d39c03cb1ad6abb51698d74a8be
paulapivat <paul.apivat@gmail.com>
2021-03-04 00:18:49 +0700

    add code to plot Jane Austen

--- text_mining/exploratory.R
+++ text_mining/exploratory.R
@@ -109,7 +109,28 @@ frequency <- bind_rows(mutate(tidy_bronte, author = "Brontë Sisters"),
     gather(author, proportion, `Brontë Sisters`:`H.G. Wells`)
 
 
-# frequency for Thai_dishes
+# NEXT STEP: plot Jane Austen
+install.packages('scales')
+library(scales)
+
+# expect a warning about rows with missing values being removed
+ggplot(frequency, aes(x = proportion, y = `Jane Austen`, 
+                      color = abs(`Jane Austen` - proportion))) +
+    geom_abline(color = "gray40", lty = 2) +
+    geom_jitter(alpha = 0.1, size = 2.5, width = 0.3, height = 0.3) +
+    geom_text(aes(label = word), check_overlap = TRUE, vjust = 1.5) +
+    scale_x_log10(labels = percent_format()) +
+    scale_y_log10(labels = percent_format()) +
+    scale_color_gradient(limits = c(0, 0.001), 
+                         low = "darkslategray4", high = "gray75") +
+    facet_wrap(~author, ncol = 2) +
+    theme(legend.position="none") +
+    labs(y = "Jane Austen", x = NULL)
+
+
+
+
+# frequency for Thai_dishes ----
 # need to include 'shared_dishes' as 'authors'
 thai_name_freq <- df %>%
     #filter(major_grouping == 'Shared dishes') %>% # or Shared dishes
@@ -122,5 +143,6 @@ thai_name_freq <- df %>%
     spread(major_grouping, proportion) %>%
     gather(major_grouping, proportion, Drinks:`Shared dishes`) 
 
-
+ggplot(thai_name_freq, aes(x = proportion, y = major_grouping),
+       color = abs(major_grouping))
 
